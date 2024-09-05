d2c7d669f10eb666df472c8a13a83528d65547ca
suckwonlee <lsw980912@gmail.com>
2024-09-05 15:39:42 +0900

    React 4장-2

--- src/chp04/Button.jsx
+++ src/chp04/Button.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './Button1.css';
-import './Button2.css'
+import './Button2.css';
+import './Button3.css';
 
 function Button(props){
     return(
--- src/chp04/ConfirmDialog.jsx
+++ src/chp04/ConfirmDialog.jsx
@@ -4,8 +4,9 @@ function ConfirmDialog(props){
     return(
         <div>
             <p>내용을 확인하셨으면 확인 버튼을 눌러주세요.</p>
-            <button1 color='green'>check</button1>
+            <button1 color='blue'>check</button1>
             <button2 color='red'>deny</button2>
+            <button3 color='green'>reset</button3>
         </div>
     )}
 export default ConfirmDialog;
