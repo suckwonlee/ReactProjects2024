b78068ab9f29fbf02ae4a50750ef0d580a2a3950
leesuckwon <lsw980912@gmail.com>
2024-11-14 14:53:50 +0900

    React 10장 숫자리스트,학생출석부

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './chp03/Book.css'
-import App from './App';
+// import './chp03/Book.css'
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Library from "./chp03/Library";
-import ConfirmDialog from "./chp04/ConfirmDialog";
-import Clock from "./chp04/Clock";
-import Commentlist from "./chp05/Commentlist";
-import NotificationList from "./chp06/NotificationList";
-import Accommodate from "./chp07/Accommodate";
-import Toggle from "./chp08/Toggle";
-import mybutton from "./chp08/Mybutton";
-import Toggle2 from "./chp08/Toggle2";
-import ClickButton from "./chp08/ClickButton";
-import Counter from "./chp08/Counter";
-import InputTest from "./chp08/inputTest";
-import ConfirmButton from "./chp08/ConfirmButton";
-import ConfirmButton3 from "./chp08/ConfirmButton3";
-import Greeting from "./chp09/Greeting";
-import MailBox from "./chp09/MailBox";
-import LoginControl2 from "./chp09/LoginControl2";
-import LandingPage from "./chp09/LandingPage";
+// import Library from "./chp03/Library";
+// import ConfirmDialog from "./chp04/ConfirmDialog";
+// import Clock from "./chp04/Clock";
+// import Commentlist from "./chp05/Commentlist";
+// import NotificationList from "./chp06/NotificationList";
+// import Accommodate from "./chp07/Accommodate";
+// import Toggle from "./chp08/Toggle";
+// import mybutton from "./chp08/Mybutton";
+// import Toggle2 from "./chp08/Toggle2";
+// import ClickButton from "./chp08/ClickButton";
+// import Counter from "./chp08/Counter";
+// import InputTest from "./chp08/inputTest";
+// import ConfirmButton from "./chp08/ConfirmButton";
+// import ConfirmButton3 from "./chp08/ConfirmButton3";
+// import Greeting from "./chp09/Greeting";
+// import MailBox from "./chp09/MailBox";
+// import LoginControl2 from "./chp09/LoginControl2";
+// import LandingPage from "./chp09/LandingPage";
+// import AppNumberList from "./chp10/AppNumberList";
+import StudentAttendance from "./chp10/StudentAttendance";
+import AppStudentAttendance from "./chp10/AppStudentAttendance";
 
 
 
@@ -29,7 +32,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
     root.render(
         <React.StrictMode>
-            <LandingPage/>
+            <AppStudentAttendance/>
         </React.StrictMode>
     );
 
